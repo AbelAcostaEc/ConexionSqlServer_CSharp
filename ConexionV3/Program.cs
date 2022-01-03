3c95b30e02f28bfe4ba6893dd94c23f53beceedd
ASAA19971a <abel.2833@hotmail.com>
2022-01-03 16:44:54 -0500

    Agregar archivos de proyecto.

---
+++ ConexionV3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+
+namespace ConexionV3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Data Source = ABEL-ASAA\\SQLEXPRESS; Integrated Security =true; Initial Catalog = Northwind";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(
+                       "SELECT CustomerID, CompanyName, ContactName " +
+                       "FROM Customers " +
+                       "WHERE Country = 'USA' " +
+                       "ORDER BY CustomerID",
+                       conn
+                    );
+
+                DataSet dataSet = new DataSet();
+                adapter.Fill(dataSet,"Clientes");
+
+                foreach (DataRow fila in dataSet.Tables["Clientes"].Rows)
+                {
+                    String nombreCliente =fila.Field<String>("CompanyName");
+                    Console.WriteLine("Cliente: " + nombreCliente);
+                }
+
+            }
+
+            
+            
+
+        }
+    }
+}
+
+
